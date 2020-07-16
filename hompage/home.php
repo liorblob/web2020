4de2a56aa0771d9bcaf3af0cbd2b7a4de68e8227
Lior Blobstein <lblobstein@paloaltonetworks.com>
2020-07-16 15:34:33 +0300

    spaces

--- hompage/home.php
+++ hompage/home.php
@@ -1,21 +1,21 @@
 <?php
- $servername = "localhost:3306";
- $username = "dbuser";
- $password = "123456";
- $dbname="noodle";
+  $servername = "localhost:3306";
+  $username = "dbuser";
+  $password = "123456";
+  $dbname="noodle";
 
- error_reporting(E_ALL ^ E_WARNING);
+  error_reporting(E_ALL ^ E_WARNING);
  
- // Create connection
- $conn = new mysqli($servername, $username, $password,$dbname);
- // Check connection
- if ($conn->connect_error) {
-    echo "<script type='text/javascript'>alert('תקלה בהתחברות למסד הנתונים');</script>";
-    die();
- }
- else {
-    $conn->query("SET NAMES 'utf8'");
- }
+  // Create connection
+  $conn = new mysqli($servername, $username, $password,$dbname);
+  // Check connection
+  if ($conn->connect_error) {
+     echo "<script type='text/javascript'>alert('תקלה בהתחברות למסד הנתונים');</script>";
+     die();
+  }
+  else {
+     $conn->query("SET NAMES 'utf8'");
+  }
 ?>
 
 <!DOCTYPE html>
